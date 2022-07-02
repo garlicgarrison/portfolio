d9a2e305ded9e4e2a3996e189ee7a0815f3726e7
epletcher72 <stevenjang75@yahoo.com>
2022-07-02 16:21:19 -0700

    links

--- src/components/cards/PaperCard.tsx
+++ src/components/cards/PaperCard.tsx
@@ -8,12 +8,15 @@ export default function PaperCard({ paper }: PaperCardProps) {
   const title = paper[0];
   const description = paper[1] as any;
   const field = paper[2];
+  const link = paper[3];
+  console.log("link", `static/problem_${link}/`);
 
   return (
     <div className={style.paper_container}>
       <h4>{title}</h4>
       <h5>{field}</h5>
       <p dangerouslySetInnerHTML={{ __html: description }}></p>
+      <Link to={`static/problem_${link}`}>See my solution</Link>
     </div>
   );
 }
---
+++ src/markdowns/about/experience3.md
@@ -0,0 +1,6 @@
+---
+type: experience
+company: Glottis Project
+project: Chrome Extension/Web App
+description: Created a Chromium plugin to add words in Netflix subtitles to the Glottis web app as flashcards. The web app will show you what words you have to study. This project was created using <b>Javascript, NextJS, Node, and Firebase</b>
+---
